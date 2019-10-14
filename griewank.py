74052a481f624db4980fe274b9325fe117542e3e
kamikazekartik <sreenivasan.kartik@gmail.com>
2019-10-13 20:05:53 -0500

    changed workflow to run all algorithms with same points
    having different random starting points was causing significant
    variance in results.

--- griewank.py
+++ griewank.py
@@ -1,13 +1,28 @@
 # %matplotlib inline
 import numpy as np
 import pandas as pd
+import matplotlib
+# matplotlib.use('pdf')
 import matplotlib.pyplot as plt
 import datetime
 import math
-plt.style.use('seaborn-white')
+# plt.style.use('seaborn-white')
 
 x_min = -20
 x_max = 20
+
+# delete these!
+mean_f_opt = []
+mean_dist_to_origin = []
+mean_dist_from_start = []
+mean_start_distance = []
+mean_iter_to_convergence = []
+experiment_list = []
+n_iter_list = []
+gamma_list = []
+alpha_list = []
+beta_list = []
+
 
 def griewank(x, gamma=4000):
     n = len(x)
@@ -81,7 +96,7 @@ def steepest_descent_griewank(x0, alpha=0.01, epsilon=1e-5, n_iter=10000, debug=
                 print("Solution found!", x_curr, griewank(x_curr, gamma), grad)
             break
         if use_ebls:
-        	alpha = ebls(x_curr)
+            alpha = ebls(x_curr)
         x_next = x_curr - alpha*grad
         x_curr = x_next
     results_df = pd.DataFrame({'x': x_vals, 'obj': obj_vals, 'grad': grad_vals})
@@ -97,11 +112,11 @@ def heavy_ball_griewank(x0, alpha=0.01, beta=0.9, epsilon=1e-5, n_iter=10000, de
     x_curr = x0
     x_prev = x0
     for i in range(n_iter):
-    	if debug:
-	        print("------------------------")
-	        print("Iteration: ", i)
-	        print(x_curr, griewank(x_curr, gamma))
-	        print("------------------------")
+        if debug:
+            print("------------------------")
+            print("Iteration: ", i)
+            print(x_curr, griewank(x_curr, gamma))
+            print("------------------------")
 
         grad = grad_griewank(x_curr, gamma)
 
@@ -151,123 +166,6 @@ def ebls(x_0, c1=0.1, c2=0.3, epsilon=1e-5, n_iter=1000, debug=False, gamma=4000
             return alpha
 
 
-def run_griewank_test(algo='steepest_descent', gamma=4000, n_iter=1000, alpha=0.01, beta=0.9, use_ebls=True):
-    results = {'x_opt': [], 'iterations': [], 'f_opt': [], 'x_0': [], 'dist_from_start': [], 'start_distance': []}
-    print "Running test.", datetime.datetime.now()
-    for i in range(n_iter):
-        # print("Random Restart: {}".format(i))
-        x_0 = np.random.uniform(low=x_min, high=x_max, size=2)
-        if algo == 'steepest_descent':
-        	x_opt, f_opt, results_df = steepest_descent_griewank(x_0, alpha=alpha, gamma=gamma, use_ebls=use_ebls)
-        elif algo == 'heavy_ball':
-        	x_opt, f_opt, results_df = heavy_ball_griewank(x_0, alpha=alpha, beta=beta, gamma=gamma, use_ebls=use_ebls)
-        elif algo == 'nesterov':
-            x_opt, f_opt, results_df = nesterov_griewank(x_0, alpha=alpha, beta=beta, gamma=gamma)
-        else:
-        	print "No such algorithm bro. The dude abides."
-        	return -1
-        results['x_opt'].append(x_opt)
-        results['iterations'].append(results_df.shape[0])
-        results['f_opt'].append(f_opt)
-        results['x_0'].append(x_0)
-        results['dist_from_start'].append(np.linalg.norm(x_opt - x_0))
-        results['start_distance'].append(np.linalg.norm(x_0))
-    results_df = pd.DataFrame(results)
-    results_df['dist_to_origin'] = results_df.apply(lambda row: np.linalg.norm(row['x_opt']), axis=1)
-    n_iter_list.append(n_iter)
-    gamma_list.append(gamma)
-    exp = algo + "_with_ebls" if use_ebls else algo + "_without_ebls" 
-    experiment_list.append(exp)
-    alpha_list.append(alpha)
-    beta_list.append(beta)
-    print "Test complete."
-    return results_df
-
-
-mean_f_opt = []
-mean_dist_to_origin = []
-mean_dist_from_start = []
-mean_start_distance = []
-mean_iter_to_convergence = []
-experiment_list = []
-n_iter_list = []
-gamma_list = []
-alpha_list = []
-beta_list = []
-
-
-def save_figure(ax, filename):
-    fig = ax.get_figure()
-    fig.savefig(filename)
-    fig.clear()
-
-def save_results(df, f_opt_df, dist_to_origin_df, dist_from_start_df, filename, hist=True):
-    df.to_csv(filename + '.csv')
-    mean_f_opt.append(df.f_opt.mean())
-    mean_dist_to_origin.append(df.dist_to_origin.mean())
-    mean_dist_from_start.append(df.dist_from_start.mean())
-    mean_start_distance.append(df.start_distance.mean())
-    mean_iter_to_convergence.append(df.iterations.mean())
-    if hist:
-        f_opt_df[filename + '_f_opt'] = df['f_opt']
-        dist_to_origin_df[filename + '_d'] = df['dist_to_origin']
-        dist_from_start_df[filename + '_d'] = df['dist_from_start']
-
-        ax = df.dist_to_origin.hist()
-        fig = ax.get_figure()
-        fig.savefig(filename + ".pdf")
-        fig.clear()
-
-def plot_aggregates(f_opt_df, dist_to_origin_df, dist_from_start_df):
-    filenames = ['f_opt_aggregated.pdf', 'dist_to_origin_aggregated.pdf']
-    for i, df in enumerate([f_opt_df, dist_to_origin_df]):
-        ax = df.plot.hist(alpha=0.5)
-        save_figure(ax, 'hist_' + filenames[i])
-        ax = df.plot.box()
-        save_figure(ax, 'box_' + filenames[i])
-
-
-def driver(n_iter=100, gamma=500, alpha=1, beta=0.99):
-    f_opt_df = pd.DataFrame()
-    dist_to_origin_df = pd.DataFrame()
-    dist_from_start_df = pd.DataFrame()
-    df = run_griewank_test('steepest_descent', gamma=gamma, n_iter=n_iter, alpha=alpha, beta=beta, use_ebls=False)
-    save_results(df, f_opt_df, dist_to_origin_df, dist_from_start_df, 'sdesc')
-    df = run_griewank_test('steepest_descent', gamma=gamma, n_iter=n_iter, alpha=alpha, beta=beta, use_ebls=True)
-    save_results(df, f_opt_df, dist_to_origin_df, dist_from_start_df, 'sdesc_ebls')
-    df = run_griewank_test('heavy_ball', gamma=gamma, n_iter=n_iter, alpha=alpha, beta=beta, use_ebls=False)
-    save_results(df, f_opt_df, dist_to_origin_df, dist_from_start_df, 'hball')
-    # df = run_griewank_test('heavy_ball', gamma=gamma, n_iter=n_iter, alpha=alpha, beta=beta, use_ebls=True)
-    # save_results(df, f_opt_df, dist_to_origin_df, dist_from_start_df, 'hball_ebls', False)
-    df = run_griewank_test('nesterov', gamma=gamma, n_iter=n_iter, alpha=alpha, beta=beta, use_ebls=False)
-    save_results(df, f_opt_df, dist_to_origin_df, dist_from_start_df, 'nesterov')
-
-    df = pd.DataFrame({'experiment': experiment_list, 'mean_f_opt': mean_f_opt, 'mean_dist_to_origin': mean_dist_to_origin,
-    					'mean_dist_from_start': mean_dist_from_start, 'mean_start_distance': mean_start_distance,
-                        'mean_iter_to_convergence': mean_iter_to_convergence, 'max_iter': n_iter_list,
-                        'gamma': gamma_list, 'alpha': alpha_list, 'beta': beta_list})
-    df.to_csv("aggregated_results.csv")
-    plot_aggregates(f_opt_df, dist_to_origin_df, dist_from_start_df)
-
-    print "Aggregated Results:"
-    print df
-    return df
-
-def beta_grid_search(n_iter=100, beta_values=None, gamma=500, alpha=1):
-    beta_values = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.85, 0.87, 0.89, 0.90, 0.91,
-            0.92, 0.93, 0.94, 0.95, 0.96, 0.97, 0.98, 0.99, 0.999]
-    results_dict = {'beta': [], 'dist_from_start': [], 'dist_to_origin': [], 'f_opt': [], 'start_distance': []}
-    for beta in beta_values:
-        print "beta:", beta
-        df = run_griewank_test('heavy_ball', gamma=gamma, n_iter=n_iter, alpha=alpha, beta=beta, use_ebls=False)
-        results_dict['beta'].append(beta)
-        results_dict['dist_from_start'].append(df.dist_from_start.mean())
-        results_dict['dist_to_origin'].append(df.dist_to_origin.mean())
-        results_dict['f_opt'].append(df.f_opt.mean())
-        results_dict['start_distance'].append(df.start_distance.mean())
-    return pd.DataFrame(results_dict)
-
-
 def get_rho_k(rho_k_minus_1 = 0):
     a = -1.0
     b = (rho_k_minus_1**2 - 1)
@@ -312,3 +210,175 @@ def nesterov_griewank(x0, alpha=1, beta=0.98, epsilon=1e-5, n_iter=1000, debug=F
         x_curr = x_next
     results_df = pd.DataFrame({'x': x_vals, 'obj': obj_vals, 'grad': grad_vals})
     return x_curr, griewank(x_curr, gamma), results_df
+
+
+def run_griewank_test(algo='steepest_descent', gamma=4000, n_iter=1000, alpha=0.01, beta=0.9, use_ebls=True):
+    results = {'x_opt': [], 'iterations': [], 'f_opt': [], 'x_0': [], 'dist_from_start': [], 'start_distance': []}
+    print "Running test.", datetime.datetime.now()
+    for i in range(n_iter):
+        # print("Random Restart: {}".format(i))
+        x_0 = np.random.uniform(low=x_min, high=x_max, size=2)
+        if algo == 'steepest_descent':
+            x_opt, f_opt, results_df = steepest_descent_griewank(x_0, alpha=alpha, gamma=gamma, use_ebls=use_ebls)
+        elif algo == 'heavy_ball':
+            x_opt, f_opt, results_df = heavy_ball_griewank(x_0, alpha=alpha, beta=beta, gamma=gamma, use_ebls=use_ebls)
+        elif algo == 'nesterov':
+            x_opt, f_opt, results_df = nesterov_griewank(x_0, alpha=alpha, beta=beta, gamma=gamma)
+        else:
+            print "No such algorithm bro. The dude abides."
+            return -1
+        results['x_opt'].append(x_opt)
+        results['iterations'].append(results_df.shape[0])
+        results['f_opt'].append(f_opt)
+        results['x_0'].append(x_0)
+        results['dist_from_start'].append(np.linalg.norm(x_opt - x_0))
+        results['start_distance'].append(np.linalg.norm(x_0))
+    results_df = pd.DataFrame(results)
+    results_df['dist_to_origin'] = results_df.apply(lambda row: np.linalg.norm(row['x_opt']), axis=1)
+    n_iter_list.append(n_iter)
+    gamma_list.append(gamma)
+    exp = algo + "_with_ebls" if use_ebls else algo + "_without_ebls" 
+    experiment_list.append(exp)
+    alpha_list.append(alpha)
+    beta_list.append(beta)
+    print "Test complete."
+    return results_df
+
+
+def save_figure(ax, filename):
+    fig = ax.get_figure()
+    fig.savefig(filename)
+    fig.clear()
+    plt.close()
+
+def save_results(df, f_opt_df, dist_to_origin_df, dist_from_start_df, filename, hist=True):
+    df.to_csv(filename + '.csv')
+    mean_f_opt.append(df.f_opt.mean())
+    mean_dist_to_origin.append(df.dist_to_origin.mean())
+    mean_dist_from_start.append(df.dist_from_start.mean())
+    mean_start_distance.append(df.start_distance.mean())
+    mean_iter_to_convergence.append(df.iterations.mean())
+    if hist:
+        f_opt_df[filename + '_f_opt'] = df['f_opt']
+        dist_to_origin_df[filename + '_d'] = df['dist_to_origin']
+        dist_from_start_df[filename + '_d'] = df['dist_from_start']
+
+        ax = df.dist_to_origin.hist()
+        fig = ax.get_figure()
+        fig.savefig(filename + ".pdf")
+        fig.clear()
+
+def plot_aggregates(f_opt_df, dist_to_origin_df, dist_from_start_df, dist_moved_towards_origin_df):
+    filenames = ['f_opt_aggregated.pdf', 'dist_to_origin_aggregated.pdf',
+                    'dist_from_start_aggregated.pdf', 'dist_moved_towards_origin.pdf']
+    for i, df in enumerate([f_opt_df, dist_to_origin_df, dist_from_start_df, dist_moved_towards_origin_df]):
+        ax = df.plot.hist(alpha=0.5)
+        save_figure(ax, 'hist_' + filenames[i])
+        ax = df.plot.box()
+        save_figure(ax, 'box_' + filenames[i])
+
+
+# @deprecated
+# I broke this!
+def driver(n_iter=100, gamma=500, alpha=1, beta=0.99):
+    f_opt_df = pd.DataFrame()
+    dist_to_origin_df = pd.DataFrame()
+    dist_from_start_df = pd.DataFrame()
+    df = run_griewank_test('steepest_descent', gamma=gamma, n_iter=n_iter, alpha=alpha, beta=beta, use_ebls=False)
+    save_results(df, f_opt_df, dist_to_origin_df, dist_from_start_df, 'sdesc')
+    df = run_griewank_test('steepest_descent', gamma=gamma, n_iter=n_iter, alpha=alpha, beta=beta, use_ebls=True)
+    save_results(df, f_opt_df, dist_to_origin_df, dist_from_start_df, 'sdesc_ebls')
+    df = run_griewank_test('heavy_ball', gamma=gamma, n_iter=n_iter, alpha=alpha, beta=beta, use_ebls=False)
+    save_results(df, f_opt_df, dist_to_origin_df, dist_from_start_df, 'hball')
+    # df = run_griewank_test('heavy_ball', gamma=gamma, n_iter=n_iter, alpha=alpha, beta=beta, use_ebls=True)
+    # save_results(df, f_opt_df, dist_to_origin_df, dist_from_start_df, 'hball_ebls', False)
+    df = run_griewank_test('nesterov', gamma=gamma, n_iter=n_iter, alpha=alpha, beta=beta, use_ebls=False)
+    save_results(df, f_opt_df, dist_to_origin_df, dist_from_start_df, 'nesterov')
+
+    df = pd.DataFrame({'experiment': experiment_list, 'mean_f_opt': mean_f_opt, 'mean_dist_to_origin': mean_dist_to_origin,
+                        'mean_dist_from_start': mean_dist_from_start, 'mean_start_distance': mean_start_distance,
+                        'mean_iter_to_convergence': mean_iter_to_convergence, 'max_iter': n_iter_list,
+                        'gamma': gamma_list, 'alpha': alpha_list, 'beta': beta_list})
+    df.to_csv("aggregated_results.csv")
+    plot_aggregates(f_opt_df, dist_to_origin_df, dist_from_start_df)
+
+    print "Aggregated Results:"
+    print df
+    return df
+
+def beta_grid_search(n_iter=100, beta_values=None, gamma=500, alpha=1):
+    beta_values = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.85, 0.87, 0.89, 0.90, 0.91,
+            0.92, 0.93, 0.94, 0.95, 0.96, 0.97, 0.98, 0.99, 0.999]
+    results_dict = {'beta': [], 'dist_from_start': [], 'dist_to_origin': [], 'f_opt': [], 'start_distance': []}
+    for beta in beta_values:
+        print "beta:", beta
+        df = run_griewank_test('heavy_ball', gamma=gamma, n_iter=n_iter, alpha=alpha, beta=beta, use_ebls=False)
+        results_dict['beta'].append(beta)
+        results_dict['dist_from_start'].append(df.dist_from_start.mean())
+        results_dict['dist_to_origin'].append(df.dist_to_origin.mean())
+        results_dict['f_opt'].append(df.f_opt.mean())
+        results_dict['start_distance'].append(df.start_distance.mean())
+    return pd.DataFrame(results_dict)
+
+
+def compare_descent_algorithms(gamma=4000, n_iter=1000, max_iter=10000, alpha=1, beta=0.98):
+    def save_results(results_list, x_0, x_opt, f_opt, iter_to_convergence):
+        results_list['dist_from_start'].append(np.linalg.norm(x_opt - x_0))
+        results_list['dist_to_origin'].append(np.linalg.norm(x_opt))
+        results_list['f_opt'].append(f_opt)
+        results_list['iter_to_convergence'].append(iter_to_convergence)
+        results_list['dist_moved_towards_origin'].append(np.linalg.norm(x_0) - np.linalg.norm(x_opt))
+
+    start_distance = []
+    sd_results = {'dist_from_start': [], 'f_opt': [], 'dist_to_origin': [], 'iter_to_convergence': [], 'dist_moved_towards_origin': []}
+    sd_ebls_results = {'dist_from_start': [], 'f_opt': [], 'dist_to_origin': [], 'iter_to_convergence': [], 'dist_moved_towards_origin': []}
+    hball_results = {'dist_from_start': [], 'f_opt': [], 'dist_to_origin': [], 'iter_to_convergence': [], 'dist_moved_towards_origin': []}
+    nesterov_results = {'dist_from_start': [], 'f_opt': [], 'dist_to_origin': [], 'iter_to_convergence': [], 'dist_moved_towards_origin': []}
+
+    for i in range(n_iter):
+        if i%100 == 0:
+            print("Random Restart: {}".format(i))
+        x_0 = np.random.uniform(low=x_min, high=x_max, size=2)
+        start_distance.append(np.linalg.norm(x_0))
+        
+        x_opt, f_opt, results_df = steepest_descent_griewank(x_0, alpha=alpha, gamma=gamma, use_ebls=False)
+        save_results(sd_results, x_0, x_opt, f_opt, results_df.shape[0])
+
+        x_opt, f_opt, results_df = steepest_descent_griewank(x_0, alpha=alpha, gamma=gamma, use_ebls=True)
+        save_results(sd_ebls_results, x_0, x_opt, f_opt, results_df.shape[0])
+        
+        x_opt, f_opt, results_df = heavy_ball_griewank(x_0, alpha=alpha, beta=beta, gamma=gamma, use_ebls=False)
+        save_results(hball_results, x_0, x_opt, f_opt, results_df.shape[0])
+        
+        x_opt, f_opt, results_df = nesterov_griewank(x_0, alpha=alpha, beta=beta, gamma=gamma, use_ebls=False)
+        save_results(nesterov_results, x_0, x_opt, f_opt, results_df.shape[0])
+
+
+    f_opt_df = pd.DataFrame()
+    dist_to_origin_df = pd.DataFrame()
+    dist_from_start_df = pd.DataFrame()
+    dist_moved_towards_origin_df = pd.DataFrame()
+
+    algo_results_map = {'sdesc': sd_results, 'sdesc_ebls': sd_ebls_results, 'heavy ball': hball_results, 'nestrov': nesterov_results}
+    aggregated_results_df = {'algo': [], 'start_distance': [], 'dist_from_start': [], 'dist_to_origin': [],
+                            'f_opt': [], 'iter_to_convergence': [], 'dist_moved_towards_origin': []}
+    for algo in algo_results_map.keys():
+        results = algo_results_map[algo]
+        aggregated_results_df['algo'].append(algo)
+        aggregated_results_df['start_distance'].append(np.mean(start_distance))
+        aggregated_results_df['dist_from_start'].append(np.mean(results['dist_from_start']))
+        aggregated_results_df['dist_to_origin'].append(np.mean(results['dist_to_origin']))
+        aggregated_results_df['f_opt'].append(np.mean(results['f_opt']))
+        aggregated_results_df['iter_to_convergence'].append(np.mean(results['iter_to_convergence']))
+        aggregated_results_df['dist_moved_towards_origin'].append(np.mean(results['dist_moved_towards_origin']))
+
+        f_opt_df[algo + '_f_opt'] = pd.Series(results['f_opt'])
+        dist_to_origin_df[algo + '_d'] = pd.Series(results['dist_to_origin'])
+        dist_from_start_df[algo + '_d'] = pd.Series(results['dist_from_start'])
+        dist_moved_towards_origin_df[algo + '_d'] = pd.Series(results['dist_moved_towards_origin'])
+        plot_aggregates(f_opt_df, dist_to_origin_df, dist_from_start_df, dist_moved_towards_origin_df)
+
+    aggregated_results_df = pd.DataFrame(aggregated_results_df)
+    print aggregated_results_df
+    return aggregated_results_df
+
